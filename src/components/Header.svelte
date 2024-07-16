23db8e8bac3468a2ebe76e43758d2163a23fbea6
Riteek Rakesh <riteek7.12@gmail.com>
2024-07-16 21:58:45 +0530

    main section fix and linked in button

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -23,7 +23,7 @@
   
     <a href="https://github.com/Riteek712" target="_blank" class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950">
       <div class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"></div>
-      <h4 class="relative z-9">Checkout Github!</h4>
+      <h4 class="relative z-9">Checkout my Github!</h4>
     </a>
   </header>
   
