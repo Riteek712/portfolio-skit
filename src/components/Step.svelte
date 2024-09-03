b0b05a47596cd20935d1664a194387b3b6af0af4
Riteek Rakesh <riteek7.12@gmail.com>
2024-09-03 22:26:32 +0530

    ui fixes and updatinfg resumee

--- src/components/Step.svelte
+++ src/components/Step.svelte
@@ -7,10 +7,10 @@
 
 <div class="container gap-16 mb-10">
 <div
-    class="p-4 sm:p-6 md:p-8 flex flex-col mr-auto gap-4 rounded-lg border border-solid border-violet-700 text-center cursor-pointer group hover:border-violet-400 duration-200"
+    class="p-4 sm:p-6 md:p-8 flex flex-col mr-auto gap-4 mr-black text-center cursor-pointer group hover:border-violet-400 duration-200"
 >
     <div
-        class="bg-slate-950 grid place-items-center px-4 text-5xl md:text-6xl -mt-10 sm:-mt-12 md:-mt-14 lg:-mt-16 mx-auto duration-200"
+        class="bg-black grid place-items-center px-4 text-5xl md:text-6xl -mt-10 sm:-mt-12 md:-mt-14 lg:-mt-16 mx-auto duration-200"
     >
         <i class={icon}></i>
     </div>
@@ -40,7 +40,9 @@
     }
     .container{
         background: rgba(0, 0, 0, 0.8);
-        box-shadow: 0px 0px 20px indigo
+        border: 5px solid black;
+        box-shadow: 0px 0px 20px indigo;
+        
     }
     .container:hover{
         box-shadow: 0px 0px 80px indigo
